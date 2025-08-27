cccd3fadd3bd9b9188b2f429e7387c5372100a6d
Hao Vu <haovu14603@gmail.com>
2025-08-27 04:59:43 -0400

    add tons

--- api/api.go
+++ api/api.go
@@ -5,7 +5,6 @@ import (
   "encoding/json"
   "crypto/tls"
   "net/http"
-  "log"
   "time"
   "io"
   "strings"
@@ -25,13 +24,21 @@ type Response struct {
 
 type PlayerStatus struct {
   Status string `json:"status"`
+  IdleTimer int `json:"idleTimer"`
 }
 
+type History struct {
+  Type string `json:"type"`
+  Songs []Response `jsons:"songs"`
+}
+var Songs []Response
 
-func SendCurrentSong(songChan chan Response) error {
+var TrackHistory History
+
+func FetchCurrentSong(songChan chan Response) error {
   var previousSong Response
   var currentSong Response
-  ticker := time.NewTicker(5000 * time.Millisecond)
+  ticker := time.NewTicker(10000 * time.Millisecond)
   
   for range ticker.C {
     tr := &http.Transport{
@@ -52,12 +59,13 @@ func SendCurrentSong(songChan chan Response) error {
 
     respData, err := io.ReadAll(resp.Body)
     if err != nil {
-      log.Fatal(err)
+      fmt.Println("Error reading json: ", err)
+      continue
     }
 
     if err := json.Unmarshal(respData, &currentSong); err != nil {
       fmt.Println("Cannot unmarshal JSON")
-      return err
+      continue
     }
     //fmt.Println(song)
     currentSong.MetaData.AlbumArtURI = strings.Replace(
@@ -67,6 +75,7 @@ func SendCurrentSong(songChan chan Response) error {
 
     if currentSong != previousSong {
       songChan <-currentSong
+      Songs = append(Songs, currentSong)
       previousSong = currentSong
     }
   }
@@ -76,7 +85,7 @@ func SendCurrentSong(songChan chan Response) error {
 func FetchCurrentStatus(statusChan chan PlayerStatus) error {
   var currentStatus PlayerStatus
   var previousStatus PlayerStatus
-  ticker := time.NewTicker(5000 * time.Millisecond)
+  ticker := time.NewTicker(10000 * time.Millisecond)
 
   for range ticker.C {
     tr := &http.Transport{
@@ -91,13 +100,14 @@ func FetchCurrentStatus(statusChan chan PlayerStatus) error {
     resp, err := client.Get(url)
 
     if err != nil {
-      log.Fatal(err)
+      fmt.Println("error getting from url:",err)
+      continue
     }
     defer resp.Body.Close()
 
     respData, err := io.ReadAll(resp.Body)
     if err != nil {
-      log.Fatal(err)
+      fmt.Println("error reading:",err)
     }
 
     if err := json.Unmarshal([]byte(respData), &currentStatus); err != nil {
@@ -108,6 +118,48 @@ func FetchCurrentStatus(statusChan chan PlayerStatus) error {
       statusChan <- currentStatus
       previousStatus = currentStatus
     }
+    if currentStatus.Status == "pause" {
+      currentStatus.IdleTimer += 2000
+    }
+    if currentStatus.Status == "play" {
+      currentStatus.IdleTimer = 0
+    }
   }
   return nil
 }
+
+func PlayerCommand(command string)error {
+  tr := &http.Transport{
+          TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+      } // 
+
+  client := &http.Client{
+    Timeout: 5 * time.Second,
+    Transport: tr,
+  }
+  var url string 
+
+  switch command {
+  case "play":
+    url = "https://10.0.0.119/httpapi.asp?command=setPlayerCmd:play"
+  case "pause":
+    url = "https://10.0.0.119/httpapi.asp?command=setPlayerCmd:pause"
+  case "onepause":
+    url = "https://10.0.0.119/httpapi.asp?command=setPlayerCmd:onepause" // toggle pause/pause
+  case "next":
+    url = "https://10.0.0.119/httpapi.asp?command=setPlayerCmd:next"
+  case "previous":
+    url = "https://10.0.0.119/httpapi.asp?command=setPlayerCmd:next"
+  }
+
+  resp, err := client.Get(url)
+  if err != nil {
+    return err
+  }
+  defer resp.Body.Close()
+  
+  return nil
+}
+
+
+
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
   "fmt"
   "net/http"
   "github.com/gorilla/websocket"
+  "wss-test/api"
+  "encoding/json"
 )
 
 var upgrader = websocket.Upgrader{
@@ -11,6 +13,7 @@ var upgrader = websocket.Upgrader{
 }
 
 var clients = make(map[*websocket.Conn]bool) //track active clients
+
 func handleConnections(w http.ResponseWriter, r *http.Request) {
 
   ws, err := upgrader.Upgrade(w, r, nil)
@@ -28,18 +31,22 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
       break
     }
     fmt.Printf("Received: %s\n", msg)
-    
-    for client := range clients {
-      if err := ws.WriteMessage(websocket.TextMessage, msg); err != nil{
-        fmt.Println("broadcast error:", err)
-        client.Close()
-        delete(clients, client)
-      }
-    }
+
+    api.PlayerCommand(string(msg))
+    handleHistory(string(msg))
   }
 }
 
 func main() {
+  songChan := make(chan api.Response)
+  statusChan := make(chan api.PlayerStatus)
+
+  go api.FetchCurrentSong(songChan)
+  go api.FetchCurrentStatus(statusChan)
+
+  go handleBroadcasting(songChan, statusChan)
+
+  http.Handle("/", http.FileServer(http.Dir("./static/")))
   http.HandleFunc("/ws", handleConnections)
 
   fmt.Println("WebSocket server started on: 8080")
@@ -48,3 +55,46 @@ func main() {
     fmt.Println("ListenAndServe:", err)
   }
 }
+
+func handleBroadcasting(songChan chan api.Response, statusChan chan api.PlayerStatus) {
+  for {
+    select {
+    case song := <-songChan:
+      jsonSong, _ := json.Marshal(song)
+      for client := range clients {
+          if err := client.WriteMessage(websocket.TextMessage, jsonSong); err != nil {
+            fmt.Println("broadcast error:", err)
+            client.Close()
+            delete(clients, client)
+          }
+      }
+    case status := <-statusChan:
+      jsonStatus, _ := json.Marshal(status)
+      for client := range clients {
+          if err := client.WriteMessage(websocket.TextMessage, jsonStatus); err != nil {
+            fmt.Println("broadcast error:", err)
+            client.Close()
+            delete(clients, client)
+          }
+        }
+    }
+  }
+}
+
+
+func handleHistory(msg string) {
+  api.TrackHistory.Type = "history"
+  api.TrackHistory.Songs = api.Songs
+  if (msg == "request"){
+    jsonHistory,_ := json.Marshal(api.TrackHistory)
+    for client := range clients {
+      if err := client.WriteMessage(websocket.TextMessage, jsonHistory); err != nil {
+              fmt.Println("broadcast error:", err)
+              client.Close()
+              delete(clients, client)
+      }
+    }
+  }
+}
+
+
